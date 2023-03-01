ede5bdfa08438a724399b78eb120c2322f2b4891
Aleksei Sharypov <a.sharypoff@gmail.com>
2023-03-01 15:20:54 +0400

    ci

--- evrone_fastapi/main.py
+++ evrone_fastapi/main.py
@@ -4,6 +4,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from .settings import settings
+import logging
 
 
 app = FastAPI()
@@ -13,6 +14,8 @@ class Status(BaseModel):
     """ Status response """
     status: str = "OK"
 
+logging.error(f"1: {settings.main_url}")
+print("1", settings.main_url)
 
 @app.get(settings.main_url)
 async def root():
